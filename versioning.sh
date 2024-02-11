91d23e3f74f57abe6a333da79ff507e64191ecad
fernando ocampo <fdocampo@gmail.com>
2024-02-11 23:39:31 +0100

    feat: new delivery

--- versioning.sh
+++ versioning.sh
@@ -11,7 +11,7 @@ increment_version() {
   array[$2]=$((array[$2]+1))
   if [ $2 -lt 2 ]; then array[2]=0; fi
   if [ $2 -lt 1 ]; then array[1]=0; fi
-  echo $(local IFS=$delimiter ; echo "${array[*]}")
+  echo $(local IFS=$delimiter ; echo "v${array[*]}")
 }
 
 increment_version $1 $2
