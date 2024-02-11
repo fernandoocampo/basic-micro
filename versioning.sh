27e204112eb9be47f0f9e213a37b79b137e630ec
fernando ocampo <fdocampo@gmail.com>
2024-02-11 15:09:09 +0100

    chore: add versioning auto

---
+++ versioning.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+### Increments the part of the string
+## $1: version itself
+## $2: number of part: 0 – major, 1 – minor, 2 – patch
+
+increment_version() {
+  local delimiter=.
+  local version=$("$1" | tr -d v)
+  local array=($(echo "$version" | tr $delimiter '\n'))
+  array[$2]=$((array[$2]+1))
+  if [ $2 -lt 2 ]; then array[2]=0; fi
+  if [ $2 -lt 1 ]; then array[1]=0; fi
+  echo $(local IFS=$delimiter ; echo "${array[*]}")
+}
+
+increment_version $1 $2
