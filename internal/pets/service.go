d17d3bfce2ff4f3efdd15795b35172a45e6fbf3d
fernando ocampo <fdocampo@gmail.com>
2024-02-18 17:49:06 +0100

    feat: add log to query

--- internal/pets/service.go
+++ internal/pets/service.go
@@ -89,6 +89,7 @@ func (s *Service) Update(ctx context.Context, pet UpdatePet) error {
 }
 
 func (s *Service) QueryByID(ctx context.Context, id PetID) (*Pet, error) {
+	s.logger.Debug("starting to query pet by id")
 	if id == EmptyPetID {
 		return nil, errEmptyPetID
 	}
